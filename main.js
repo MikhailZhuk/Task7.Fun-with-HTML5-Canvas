55e27319913dfd0de87a60af40f25403bd7ddd03
Mikhail Zhuk <miha-44@mail.ru>
2020-12-15 15:24:00 +0300

    feat: add change color

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const color = document.getElementById('base');
+const space = document.getElementById('spacing');
 ctx.strokeStyle = "#365eec";
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 100;
-// ctx.globalCompositeOperation = 'multifly';
-
+ctx.globalCompositeOperation = 'multifly';
 
 let isDrawing = false;
 let lastX = 0;
@@ -18,13 +19,20 @@ let direction = true;
 function draw(e) {
   if(!isDrawing) return;
    console.log(e);
-   ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+   ctx.lineWidth = space.value;
+   ctx.strokeStyle = color.value;
    ctx.beginPath();
    ctx.moveTo(lastX, lastY);
    ctx.lineTo(e.offsetX, e.offsetY);
    ctx.stroke();   
    [lastX, lastY] = [e.offsetX, e.offsetY] 
   
+  ctx.strokeStyle =`hsl(${hue}, 100%, 50%)`; 
+  ctx.beginPath();
+   ctx.moveTo(lastX, lastY);
+   ctx.lineTo(e.offsetX, e.offsetY);
+   ctx.stroke();   
+   [lastX, lastY] = [e.offsetX, e.offsetY]  
   hue++;
   if(hue >= 360) {
     hue = 0;
